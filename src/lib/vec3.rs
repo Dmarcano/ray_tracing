675f6801d85d3fa5cb490a30b6b5b53a955e7913
Dmarcano <dmarcan1@swarthmore.edu>
2021-02-07 18:12:35 -0500

    initialization of vector class

---
+++ src/lib/vec3.rs
@@ -0,0 +1,50 @@
+
+use std::ops::{Add, AddAssign, Mul, MulAssign,  Sub, SubAssign,  Div, DivAssign,  Neg};
+use num_traits::identities::Zero; 
+
+/// A generic Number which can be assigned, 
+pub trait Number :  Zero + Add + Mul + Sub + Div + Neg + AddAssign + MulAssign + SubAssign + DivAssign{ 
+
+}
+
+pub struct Vec3<T> 
+where T : Number
+{ 
+    x : T, 
+    y : T, 
+    z : T 
+}
+
+
+impl<T> Vec3<T> 
+where T :Number
+{ 
+
+    pub fn new(x : T, y : T , z: T) -> Vec3<T> { 
+        Vec3 { 
+            x, 
+            y, 
+            z
+        }
+    }
+
+    pub fn zero() -> Vec3<T> { 
+        Vec3 { 
+            x : T::zero(), 
+            y: T::zero(), 
+            z: T::zero()
+        }
+    }
+}
+
+impl<T : Number> Add for Vec3<T> {
+    type Output = Vec3<T>;
+
+    fn add(self, other:Self) -> Self {
+        Self { 
+            x : self.x + other.x, 
+            y : self.y + other.y, 
+            z : self.z + other.z
+        }
+    }
+}
