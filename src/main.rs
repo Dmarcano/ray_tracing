d922ccebe9daaaa45d5205337298cd5db310b84d
Diego <marcanp.diego@gmail.com>
2021-02-14 11:33:36 -0500

    separate main

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,19 @@ use lib::canvas::{Canvas, CanvasRender, PpmFile};
 fn main() {
     let img_width : usize= 256; 
     let img_height : usize = 256; 
-
     let mut out_file = PpmFile::create("public/out.ppm").unwrap(); 
-
-
     let mut canvas = Canvas::<u8>::new(img_width, img_height);
-    
+
+    draw_gradient(&mut canvas);
+    out_file.render(&canvas).unwrap(); 
+}
+
+
+
+fn draw_gradient(canvas : &mut Canvas<u8>) { 
+    let img_height = canvas.height; 
+    let img_width = canvas.width; 
+
     for i in (0..img_height).rev() { 
         for j in 0..img_width { 
             let red = i as f64/ (img_width - 1) as f64; 
@@ -23,11 +30,7 @@ fn main() {
                 y : (blue *255.999) as u8,
                 z : green as u8
             }; 
-
             canvas.buffer[i][j] = color;
-
         };
     };
-
-    out_file.render(&canvas).unwrap(); 
 }
