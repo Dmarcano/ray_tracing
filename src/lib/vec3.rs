88c5a689bdb47af9974bc92faa429ecd80c615fd
Dmarcano <dmarcan1@swarthmore.edu>
2021-02-19 18:00:33 -0500

    proper vector multiplication and division

--- src/lib/vec3.rs
+++ src/lib/vec3.rs
@@ -7,34 +7,6 @@ pub trait Number :  Zero + Add + Mul<Output = Self> + Sub<Output = Self> + Div<O
 
 }
 
-// A blank implementation of 
-macro_rules! number_impl {
-    ($($t:ty)*) => ($(
-        impl Number for $t { }
-    )*)
-}
-number_impl!{u8 u16 u32 u64 usize f32 f64  i8 i16 i32 i64}
-
-macro_rules! ops_assign_impl {
-    ($operand: tt , $trait_name : ident, $fn_name : ident ) => {
-
-        impl<T: Number> $trait_name for Vec3<T> { 
-            fn $fn_name(&mut self, other : Self) {
-                self.x $operand other.x; 
-                self.y $operand other.y; 
-                self.z $operand other.z; 
-            }
-        }
-    };
-}   
-
-ops_assign_impl!(*=, MulAssign, mul_assign);
-ops_assign_impl!(+=, AddAssign, add_assign); 
-ops_assign_impl!(/=, DivAssign, div_assign); 
-ops_assign_impl!(-=, SubAssign, sub_assign); 
-
-
-
 #[derive(PartialEq, PartialOrd, Eq, Ord, Debug, Copy, Clone)]
 /// A struct that represents some vector which has 3Dimensional quantities. 
 pub struct Vec3<T> 
@@ -49,6 +21,31 @@ where T : Number
 pub type Color = Vec3<u8>;
 
 // pub type Point = Vec3<f64>;
+
+// A blank implementation of 
+macro_rules! number_impl {
+    ($($t:ty)*) => ($(
+        impl Number for $t { }
+    )*)
+}
+number_impl!{u8 u16 u32 u64 usize f32 f64  i8 i16 i32 i64}
+
+// To assign an operator is to always 
+macro_rules! ops_assign_impl {
+    ($operand: tt, $trait_name : ident, $fn_name : ident ) => {
+        impl<T: Number> $trait_name for Vec3<T> { 
+            fn $fn_name(&mut self, other : Self) {
+                self.x $operand other.x; 
+                self.y $operand other.y; 
+                self.z $operand other.z; 
+            }
+        }
+    };
+}   
+
+ops_assign_impl!(+=, AddAssign, add_assign); 
+ops_assign_impl!(-=, SubAssign, sub_assign); 
+
 
 impl<T : Number> Vec3<T> { 
 
@@ -87,7 +84,7 @@ impl<T : Number> Vec3<T> {
 
 impl Vec3<f64> { 
     pub fn len(&self) -> f64 { 
-        self.len().sqrt()
+        (self.len() as f64).sqrt()
     }
 
     pub fn unit_vec(&self) -> Self { 
@@ -101,6 +98,7 @@ impl Vec3<f64> {
     }
 }
 
+// vector addition
 impl<T : Number> Add for Vec3<T> {
     type Output = Self;
 
@@ -113,6 +111,7 @@ impl<T : Number> Add for Vec3<T> {
     }
 }
 
+// divide a vector by a scalar
 impl<T: Number> Sub for Vec3<T> { 
     type Output = Self; 
 
@@ -125,28 +124,46 @@ impl<T: Number> Sub for Vec3<T> {
     }
 }
 
-impl <T : Number> Mul for Vec3<T> { 
-    type Output = Self; 
-    
-    fn mul(self, other: Self) ->Self::Output { 
+// scale a vector by a scalar
+impl<T: Number> Mul<T> for Vec3<T> { 
+    type Output = Self;
+
+    fn mul(self, scalar: T) ->Self::Output {
         Self { 
-            x : self.x * other.x, 
-            y : self.y * other.y, 
-            z : self.z * other.z
+            x : self.x * scalar, 
+            y: self.y * scalar, 
+            z : self.z * scalar
         }
     }
 }
 
-impl <T: Number> Div for Vec3<T> { 
+impl<T: Number> MulAssign<T> for Vec3<T> { 
+    
+fn mul_assign(&mut self, scalar: T) {
+        self.x *= scalar; 
+        self.y *= scalar; 
+        self.z *= scalar; 
+    }
+}
+
+impl <T: Number> Div<T> for Vec3<T> { 
     type Output = Self; 
 
-    fn div(self, other: Self) -> Self::Output { 
+    fn div(self, scalar: T) -> Self::Output { 
 
         Self { 
-            x : self.x / other.x, 
-            y : self.y / other.y, 
-            z : self.z / other.z,  
+            x : self.x / scalar, 
+            y : self.y / scalar, 
+            z : self.z / scalar,  
         }
+    }
+}
+
+impl<T: Number> DivAssign<T> for Vec3<T> { 
+    fn div_assign(&mut self, scalar: T) { 
+        self.x /= scalar;
+        self.y /= scalar; 
+        self.z /= scalar; 
     }
 }
 
@@ -185,25 +202,29 @@ mod test{
     #[test]
     fn mul_test() {
         let mut first = Vec3::<u8>::new(1, 2, 3); 
-        let second = Vec3::<u8>::new(2, 2, 2); 
-        let expected = Vec3::<u8>::new(2, 4, 6); 
-
-        let mul_out = first * second; 
-        assert_eq!(mul_out, expected); 
-        first *= second; 
+        let scalar : u8= 2; 
+        let expected = Vec3::<u8>::new(2, 4, 6);
+        
+        let out =   first*scalar; 
+        assert_eq!(out, expected); 
+        first *= scalar; 
         assert_eq!(first, expected); 
     }
 
     #[test]
     fn div_test() {
-        let mut first = Vec3::<u8>::new(3, 6, 9); 
-        let expected = Vec3::<u8>::new(1,1,1); 
-
-        let out = first / first ; 
+        let mut first = Vec3::<u8>::new(2, 4, 6);
+        let expected = Vec3::<u8>::new(1, 2, 3); 
+        let scalar : u8= 2; 
+        
+        let out = first/scalar; 
         assert_eq!(out, expected); 
-        first /= first.clone(); 
-        assert_eq!(first, expected)
+        first /= scalar; 
+        assert_eq!(first, expected); 
+
     }
+
+    
 
     #[test]
     fn dot_test() {
