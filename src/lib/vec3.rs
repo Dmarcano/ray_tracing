dbdf86918de790b064b25789ed85bd931f431ff5
Dmarcano <dmarcan1@swarthmore.edu>
2021-02-19 01:24:11 -0500

    finish vector3 operand implementations

--- src/lib/vec3.rs
+++ src/lib/vec3.rs
@@ -13,9 +13,29 @@ macro_rules! number_impl {
         impl Number for $t { }
     )*)
 }
-
 number_impl!{u8 u16 u32 u64 usize f32 f64  i8 i16 i32 i64}
 
+macro_rules! ops_assign_impl {
+    ($operand: tt , $trait_name : ident, $fn_name : ident ) => {
+
+        impl<T: Number> $trait_name for Vec3<T> { 
+            fn $fn_name(&mut self, other : Self) {
+                self.x $operand other.x; 
+                self.y $operand other.y; 
+                self.z $operand other.z; 
+            }
+        }
+    };
+}   
+
+ops_assign_impl!(*=, MulAssign, mul_assign);
+ops_assign_impl!(+=, AddAssign, add_assign); 
+ops_assign_impl!(/=, DivAssign, div_assign); 
+ops_assign_impl!(-=, SubAssign, sub_assign); 
+
+
+
+#[derive(PartialEq, PartialOrd, Eq, Ord, Debug, Copy, Clone)]
 /// A struct that represents some vector which has 3Dimensional quantities. 
 pub struct Vec3<T> 
 where T : Number
@@ -96,4 +116,61 @@ impl <T: Number> Div for Vec3<T> {
             z : self.z / other.z,  
         }
     }
+}
+
+#[cfg(test)]
+mod test{ 
+
+    use super::*; 
+
+    #[test]
+    fn add_test() {
+        let mut first = Vec3::<u8>::new(1, 2, 3); 
+        let second = Vec3::<u8>::new(4, 5, 6);
+
+        let expected = Vec3::<u8>::new(5, 7, 9);
+
+        let add_out = first + second; 
+
+        assert_eq!(add_out, expected); 
+        first += second; 
+        assert_eq!(first, expected); 
+
+    }
+
+    #[test]
+    fn sub_test() {
+        let mut first = Vec3::<u8>::new(1, 2, 3); 
+        let second = first.clone(); 
+        let expected = Vec3::<u8>::zero(); 
+
+        let sub_out = first - second; 
+        assert_eq!(sub_out, expected); 
+        first -= second; 
+        assert_eq!(first, expected)
+    }
+
+    #[test]
+    fn mul_test() {
+        let mut first = Vec3::<u8>::new(1, 2, 3); 
+        let second = Vec3::<u8>::new(2, 2, 2); 
+        let expected = Vec3::<u8>::new(2, 4, 6); 
+
+        let mul_out = first * second; 
+        assert_eq!(mul_out, expected); 
+        first *= second; 
+        assert_eq!(first, expected); 
+    }
+
+    #[test]
+    fn div_test() {
+        let mut first = Vec3::<u8>::new(3, 6, 9); 
+        let expected = Vec3::<u8>::new(1,1,1); 
+
+        let out = first / first ; 
+        assert_eq!(out, expected); 
+        first /= first.clone(); 
+        assert_eq!(first, expected)
+    }
+
 }
